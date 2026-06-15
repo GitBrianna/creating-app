5d4fd65f82b2782eaa482d9ebe7f07eb33e8474e
GitBrianna <briannachannnel@gmail.com>
2026-06-15 03:28:07 -0400

    coin tossing spaced

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10) start_button = st.button('Run')
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10) 
+
+start_button = st.button('Run')
 
 if start_button:
     st.write(f'Runningn the experiment of {number_of_trials} trials.')
